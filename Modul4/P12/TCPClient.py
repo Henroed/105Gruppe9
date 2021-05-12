ad8f6af69bee27d77c8d7491839e10ce6e544bfc
Bjørnar Sømme <bjorsom@icloud.com>
2021-05-12 20:31:30 +0200

    HEI

--- Modul4/P12/TCPClient.py
+++ Modul4/P12/TCPClient.py
@@ -1,10 +1,25 @@
-from socket import *
-serverName = 'servername'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = raw_input('Input lowercase sentence:')
-clientSocket.send(sentence)
-modifiedSentence = clientSocket.recv(1024)
-print ('From Server:', )
-modifiedSentenceclientSocket.close()
+import socket
+
+
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
