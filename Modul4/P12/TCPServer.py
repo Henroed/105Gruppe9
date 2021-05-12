a5516a3372c46ef05f37855314aa1f855ad13dba
bjornarsomme <79580246+bjornarsomme@users.noreply.github.com>
2021-05-12 20:44:00 +0200

    Update TCPServer.py

--- Modul4/P12/TCPServer.py
+++ Modul4/P12/TCPServer.py
@@ -2,9 +2,10 @@ import socket
 
 
 def server_program():
-    
-    host = localhost
-    port = 80
+    # get the hostname
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
+
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
